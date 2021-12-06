4ea5d4e4c184532d8e25bf63e1603b14e3af349c
foresightyj <imyuanjian@gmail.com>
2021-12-06 09:41:21 +0800

    minor

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 import { interval } from "rxjs";
 import { scan } from "rxjs/operators";
 
-const source = interval(2000);
+const source = interval(1000);
 const messageElement = document.querySelector(".message");
 
 const log = (x) => (messageElement.textContent = x);
 
 source
 	.pipe(scan((acc) => acc + 1, 0))
-	.subscribe((count) => log(count));
+	.subscribe((count) => log(count + " seconds"));
